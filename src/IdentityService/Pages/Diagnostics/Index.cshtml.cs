82aa2f63ca809f22ec00825d949529ad8fb227d7
dorume-abstract <savitskiy.danilo.everything@gmail.com>
2024-07-02 22:07:34 +0300

    dockerize services

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -12,23 +12,23 @@ namespace IdentityService.Pages.Diagnostics;
 [Authorize]
 public class Index : PageModel
 {
-    public ViewModel View { get; set; } = default!;
+  public ViewModel View { get; set; } = default!;
 
-    public async Task<IActionResult> OnGet()
+  public async Task<IActionResult> OnGet()
+  {
+    var localAddresses = new List<string?> { "::ffff:172.21.0.1", "127.0.0.1", "::1" };
+    if (HttpContext.Connection.LocalIpAddress != null)
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
-        if(HttpContext.Connection.LocalIpAddress != null)
-        {
-            localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
-        }
-
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
-        {
-            return NotFound();
-        }
-
-        View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
-        return Page();
+      localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
     }
+
+    if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
+    {
+      return NotFound();
+    }
+
+    View = new ViewModel(await HttpContext.AuthenticateAsync());
+
+    return Page();
+  }
 }
