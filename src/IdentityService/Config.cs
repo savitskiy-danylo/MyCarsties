d57b39cacb418cb938c3c9acc0da0d58d28464c2
dorume-abstract <savitskiy.danilo.everything@gmail.com>
2024-07-07 16:55:04 +0300

    add NextAuth

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -39,7 +39,8 @@ public static class Config
           AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
           RequirePkce = false,
           AllowOfflineAccess = true,
-          AccessTokenLifetime = 36000*24*30
+          AccessTokenLifetime = 36000*24*30,
+          AlwaysIncludeUserClaimsInIdToken = true
         }
       };
 }
