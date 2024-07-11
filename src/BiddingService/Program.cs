ad57bb42247c8d8dbff936f7454a3bff0fd39271
dorume-abstract <savitskiy.danilo.everything@gmail.com>
2024-07-11 18:53:17 +0300

    add bid service

---
+++ src/BiddingService/Program.cs
@@ -0,0 +1,54 @@
+using System.Reflection;
+using BiddingService.Consumers;
+using BiddingService.RequestHelpers;
+using BiddingService.Services;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+  x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+  x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+    {
+      host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+      host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+    });
+
+    cfg.ConfigureEndpoints(context);
+  });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(opt =>
+  {
+    opt.Authority = builder.Configuration["IdentityServiceUrl"];
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters.ValidateAudience = false;
+    opt.TokenValidationParameters.NameClaimType = "username";
+  });
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHostedService<CheckAuctionFinished>();
+builder.Services.AddScoped<GrpcAuctionClient>();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync(
+  "BidDb",
+  MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
+
+app.Run();
